8ab560f9d91e4a2bb593f28107363f30b3ec34e5
Varun Agrawal <varagrawal@gmail.com>
2021-08-05 11:22:27 -0400

    speed up boost install

--- .github/scripts/boost.sh
+++ .github/scripts/boost.sh
@@ -9,10 +9,10 @@ tar -zxf ${BOOST_FOLDER}.tar.gz
 
 # Bootstrap
 cd ${BOOST_FOLDER}/
-./bootstrap.sh
+./bootstrap.sh --prefix=$CURRDIR/boost_install --with-libraries=serialization,filesystem,thread,system,atomic,date_time,timer,chrono,program_options,regex
 
 # Build and install
-sudo ./b2 install
+sudo ./b2 -j$(sysctl -n hw.logicalcpu) install
 
 # Rebuild ld cache
 sudo ldconfig
